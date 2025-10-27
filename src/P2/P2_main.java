e03d4ba72bffe5893ee817959c07f84e612868e5
fcrt5rp9wb-netizen <fcrt5rp9wb@privaterelay.appleid.com>
2025-10-27 14:34:58 +0100

    Zahlen und Wahrheitswerte

---
+++ src/P2/P2_main.java
@@ -0,0 +1,16 @@
+package P2;
+
+public class P2_main {
+    
+    public static void main(String[] args) {
+        int i = 12;
+        boolean b = false; // b direkt initialisieren
+        
+        if (i > 10) {
+            b = true;
+        }
+
+        // Jetzt kannst du b sicher verwenden, z.B.:
+        System.out.println(b); 
+    }
+}
--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class P1_main {
 	{
-	Scanner s = new Scanner(System.in);
-	String zeile = new String();
-	zeile = s.nextLine();
-	System.out.println(zeile);
-	s.close();
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
 
-}}
+	}
+}
